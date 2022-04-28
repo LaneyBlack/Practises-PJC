f95ed615cb77a05294d664422c4d55dc3acb547d
Eugene <eugene.lange2@gmail.com>
2022-04-28 20:29:21 +0200

    C6 done.

--- c6/s24382.cpp
+++ c6/s24382.cpp
@@ -14,7 +14,7 @@ void fun(const std::vector<int> &v, int **min, int *&max) {
 }
 
 //Ex2
-char fromIntToLetter (int value) {
+char fromIntToLetter(int value) {
     if (value == 31)
         return '_';
     return (64 + value);
@@ -24,7 +24,7 @@ void transcribeFromInt(int value) {
     // 0000 0000 0000 0000 0000 0000 0000 0000 - int
     // 00 00000 00000 00000 00000 00000 00000 - our
     for (int i = 5; i >= 0; i--) {
-        cout << fromIntToLetter(0b11111 & (value >> ((i*5))));
+        cout << fromIntToLetter(0b11111 & (value >> ((i * 5))));
     }
     cout << endl;
 }
@@ -34,9 +34,8 @@ int main() {
     int min = vec[0], max = vec[0], *pmin = &min, *pmax = &max;
     fun(vec, &pmin, pmax);
     cout << "[ ";
-    for (int i = 0; i < vec.size(); ++i) {
+    for (int i = 0; i < vec.size(); ++i)
         cout << vec[i] << ' ';
-    }
     cout << "]\nMin " << min << endl;
     cout << "Max " << max << endl;
     int value1 = 278905155; // 0b00_01000_01001_11111_10000_01010_00011;
