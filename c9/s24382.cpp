72b5fdf84f594d58f14ff89384f28df73efe3e90
Eugene <eugene.lange2@gmail.com>
2022-05-20 00:03:49 +0200

    C9 init

--- c9/s24382.cpp
+++ c9/s24382.cpp
@@ -6,12 +6,81 @@
 
 using namespace std;
 
-struct str {
-    int x;
-    int y;
+struct Person {
+    string name;
+    int birthYear;
 };
 
+bool compare (Person person1, Person person2) {
+    return person1.birthYear = person2.birthYear && person1.name == person2.name;
+}
+
+struct Node {
+    double data;
+    Node * next;
+};
+bool atEnd = false;
+void addNode(Node *& head, double data) {
+    Node* add = new Node();
+    add->data = data;
+    if(atEnd && head){
+        Node * tmp = head;
+        while(tmp->next){
+            tmp = tmp->next;
+        }
+        tmp->next = add;
+        atEnd = false;
+    } else {
+        add->next = head;
+        head = add;
+        atEnd = true;
+    }
+}
+
+const Node * findMax(const Node * head){
+    const Node * tmp = head;
+    const Node * max = head;
+    while (tmp){
+        if (tmp->data > max->data)
+            max = tmp;
+        tmp = tmp->next;
+    }
+    return max;
+}
+
+void printList(const Node * head) {
+    const Node * tmp = head;
+    cout << "[ ";
+    while (tmp){
+        cout << tmp->data << " ";
+        tmp = tmp->next;
+    }
+    cout << " ]" << endl;
+}
+
 int main(){
-    str r = {1,2};
+    cout << "------------W6------------" << endl;
+    cout << (compare({"Peter",1988}, {"Peter",1988})?"true" : "false") << endl;
+    cout << (compare({"Peter",1988}, {"John",1974})?"true" : "false") << endl;
+    cout << "------------W7------------" << endl;
+    Node * head = 0;
+    addNode(head, 2.6);
+    addNode(head, 3.8);
+    addNode(head, 7.4);
+    addNode(head, 2.2);
+    addNode(head, 6.3);
+    printList(head);
+    const Node * max = findMax(head);
+    if (max)
+        cout << "Max : " << max->data << endl;
+    else
+        cout << "Max is null" << endl;
+
+    head = 0;
+    max = findMax(head);
+    if (max)
+        cout << "Max : " << max->data << endl;
+    else
+        cout << "Max is null" << endl;
     return 0;
 }
