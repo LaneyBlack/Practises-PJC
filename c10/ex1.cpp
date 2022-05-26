789288eae514f51dbf275c0a46f581a74f072953
Laney Black <laney.black.74@gmail.com>
2022-05-26 23:02:17 +0200

    C10 done

--- c10/ex1.cpp
+++ c10/ex1.cpp
@@ -1,13 +1,44 @@
 #include <iostream>
-#include <cmath>
-#include <string.h>
-//s24382
 using namespace std;
 
 class Resistor {
+    double value;
+public:
+    Resistor(){
+        value=0;
+    };
 
+    Resistor(double value){
+        this->value = value;
+    };
+
+    double r() const {
+        return value;
+    }
+
+    void r(double value){
+        this->value = value;
+    }
 };
-int main (){
 
+Resistor operator+(const Resistor&r, const Resistor& r1){
+    Resistor result;
+    return result = r.r()+ r1.r();
+}
+
+Resistor operator*(const Resistor& r, const Resistor& r1 ){
+    Resistor result;
+    return result = (r.r() * r1.r()) / (r.r()+r1.r());
+}
+
+ostream & operator << (ostream& stream, const Resistor& r){
+    return stream << r.r();
+}
+
+int main (){
+    Resistor r1, r2{7};
+    r1.r(4);
+    cout << "Sum of resistors: " <<(r1 + r2) << endl;
+    cout << "Multiply resistors: " <<(r1 * r2) << endl;
     return 0;
 }
---
+++ c10/ex2.cpp
@@ -0,0 +1,84 @@
+#include <iostream>
+#include <math.h>
+
+using namespace std;
+
+class Segment {
+    double A, B;
+public:
+    Segment(double A, double B) : A(A), B(B) {}
+
+    friend Segment operator*(Segment seg, double i);
+
+    friend Segment operator*(double i, Segment seg);
+
+    friend Segment operator/(Segment seg, double i);
+
+    friend Segment operator-(Segment seg, double i);
+
+    friend Segment operator+(Segment seg, double i);
+
+    friend Segment operator+(double i, Segment);
+
+    friend std::ostream &operator<<(std::ostream &stream, const Segment m);
+
+    friend Segment operator+(Segment seg, Segment i);
+
+    bool operator()(double i) {
+        if (i > this->A && i < this->B)
+            return true;
+        else
+            return false;
+    }
+};
+
+Segment operator*(Segment seg, double i) {
+    seg.A *= i;
+    seg.B *= i;
+    return seg;
+}
+
+Segment operator*(double i, Segment seg) {
+    seg.A *= i;
+    seg.B *= i;
+    return seg;
+}
+
+Segment operator/(Segment seg, double i) {
+    seg.A /= i;
+    seg.B /= i;
+    return seg;
+}
+
+Segment operator-(Segment seg, double i) {
+    seg.A -= i;
+    seg.B -= i;
+    return seg;
+}
+
+Segment operator+(Segment seg, double i) {
+    seg.A += i;
+    seg.B += i;
+    return seg;
+}
+
+Segment operator+(double i, Segment seg) {
+    seg.A += i;
+    seg.B += i;
+    return seg;
+}
+
+Segment operator+(Segment seg, Segment i) {
+    return *(new Segment((std::min(seg.A, i.A)), std::max(seg.B, i.B)));
+}
+
+std::ostream &operator<<(std::ostream &stream, const Segment m) {
+    return stream << "[" << m.A << "," << m.B << "]";
+}
+
+int main() {
+    Segment seg{2, 3}, s = 1 + 2 * ((seg - 2) / 2 + seg) / 3;
+    cout << s << endl << boolalpha;
+    for (double x = 0.5; x < 4; x += 1)
+        cout << "x=" << x << ": " << s(x) << endl;
+}
